49691545379265448450bbc2775545a87b897f16
manningt <tom@manningetal.com>
2024-12-12 12:14:40 -0500

    updated to use make_mp3_file function

--- do_all_rows.py
+++ do_all_rows.py
@@ -5,74 +5,48 @@ This program reads a csv file that has columns: number,file name,words
 For each row, it sends the 'words' text to Polly, gets a MP3 back, and names the MP3 file as per the file name column.
 
 """
-try:
-    from boto3 import Session
-except:
-   print("import boto3 failed: did you do?: source ./venv/bin/activate")
-   exit(1)
 
-from botocore.exceptions import BotoCoreError, ClientError
-from contextlib import closing
+import csv
 import sys
-from tempfile import gettempdir
-
-# Create a client using the credentials and region defined in the [adminuser]
-# section of the AWS credentials file (~/.aws/credentials).
-session = Session(profile_name="default")
-polly = session.client("polly")
-
-phrases_file = "boomer_wav_files.csv"
-# phrases_file = "demo_announcements.csv"
-# phrases_file = "test_wav_files.csv"
-output_dir = "/home/pi/repos/audio/"
-# output_dir = "/home/pi/audiofiles/test/"
-
-# The following was for newscaster type voice - not used, slow was preferred:
-# ssml_leading_tags= '<speak><amazon:domain name="news"><prosody rate="slow">'
-# ssml_trailing_tags= '</prosody></amazon:domain></speak>'
-
-ssml_leading_tags= '<speak><prosody rate="slow">'
-ssml_trailing_tags= '</prosody></speak>'
-selected_voice= "Stephen" #other choices: Matthew, Joanna
-
-def make_mp3_file(filename, words):
-    try:
-        response = polly.synthesize_speech(Text=words, OutputFormat="mp3", 
-                                            SampleRate="22050", Engine="neural",
-                                            VoiceId=selected_voice, TextType="ssml")
-    except (BotoCoreError, ClientError) as error:
-        # The service returned an error, exit gracefully
-        print(error)
-        sys.exit(-1)
-
-    # Access the audio stream from the response
-    if "AudioStream" in response:
-        # Note: Closing the stream is important because the service throttles on the
-        # number of parallel connections. Here we are using contextlib.closing to
-        # ensure the close method of the stream object will be called automatically
-        # at the end of the with statement's scope.
-            with closing(response["AudioStream"]) as stream:
-                output = f"{output_dir}{filename[:-4]}.mp3"            
-
-                try:
-                    # Open a file for writing the output as a binary stream
-                    with open(output, "wb") as file:
-                        file.write(stream.read())
-                except IOError as error:
-                    # Could not write to file, exit gracefully
-                    print(error)
-                    sys.exit(-1)
-
-    else:
-        # The response didn't contain audio data, exit gracefully
-        print("Could not stream audio")
-        sys.exit(-1)
+import os
+import argparse
+from pathlib import Path
+from make_mp3 import make_mp3_file
 
 if __name__ == "__main__":
-    import csv
-    with open(phrases_file) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            # print(row['file name'], row['words'])
-            ssml= f'{ssml_leading_tags}{row["words"]}{ssml_trailing_tags}'
-            make_mp3_file(filename=row['file name'], words=ssml)
+
+   phrases_file= None
+
+   argParser = argparse.ArgumentParser()
+   argParser.add_argument("input", type=str, help="input csv filename")
+
+   args = argParser.parse_args()
+   # print(f'\n\t{args.input= } {args.auth_path= }\n\t{args.dont_email= } {args.parse_only= }')
+
+   if args.input is None:
+      use_boomer_wav_csv = input("Do you want to generate MP3s from 'boomer_wav_files.csv' (Y/N): ")
+      if use_boomer_wav_csv.lower() == 'y':
+         phrases_file = "boomer_wav_files.csv"
+      else:
+         use_boomer_wav_csv = input("Do you want to generate MP3s from 'demo_announcements.csv' (Y/N): ")
+         if use_boomer_wav_csv.lower() == 'y':
+            phrases_file = "demo_announcements.csv"
+         else:
+            printf("Quiting because a file was not selected.")
+            sys.exit(1)
+   elif Path(args.input).is_file():
+      phrases_file = args.input
+   else:
+      sys.exit("file selected is not a file.")
+
+   with open(phrases_file) as csvfile:
+      reader = csv.DictReader(csvfile)
+      for row in reader:
+         print(f"{row['file name']}  words: {row['words']}")
+         base_filename= row['file name'][:-4]
+         return_tuple= make_mp3_file(base_filename, row['words'])
+         if return_tuple[0]:
+               print(f"added file '{base_filename}.mp3' with tts for '{row['words']}'")
+         else:
+               print(f"{return_tuple[1]}")
+
